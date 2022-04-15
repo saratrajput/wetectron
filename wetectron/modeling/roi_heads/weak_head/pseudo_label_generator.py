0238edbae9a46fb5658f02ea6370b4dcbbf16a6c
saratrajput <pattarsuraj@gmail.com>
2022-04-15 22:27:18 +0900

    :hammer: Helps to train on custom data. Check if indices are zero before taking the first index in box proposal.

--- wetectron/modeling/roi_heads/weak_head/pseudo_label_generator.py
+++ wetectron/modeling/roi_heads/weak_head/pseudo_label_generator.py
@@ -23,56 +23,63 @@ class mist_layer(object):
     @torch.no_grad()
     def __call__(self, proposals, source_score, labels, device, return_targets=False):
         num_rois = len(proposals)
+        # print("Num rois: {}".format(num_rois))
         k = int(num_rois * self.portion)
+        # print("K: {}".format(k))
         num_gt_cls = labels[1:].sum()
-        if num_gt_cls != 0 and num_rois != 0:
-            cls_prob = source_score[:, 1:]
-            gt_cls_inds = labels[1:].nonzero(as_tuple=False)[:, 0]
-            sorted_scores, max_inds = cls_prob[:, gt_cls_inds].sort(dim=0, descending=True)
-            sorted_scores = sorted_scores[:k]
-            max_inds = max_inds[:k]
+        if k != 0:
+            if num_gt_cls != 0 and num_rois != 0:
+                cls_prob = source_score[:, 1:]
+                gt_cls_inds = labels[1:].nonzero(as_tuple=False)[:, 0]
+                sorted_scores, max_inds = cls_prob[:, gt_cls_inds].sort(dim=0, descending=True)
+                sorted_scores = sorted_scores[:k]
+                max_inds = max_inds[:k]
 
-            _boxes = proposals.bbox[max_inds.t().contiguous().view(-1)].view(num_gt_cls.int(), -1, 4)
-            _boxes = BatchBoxList(_boxes, proposals.size, mode=proposals.mode)
-            ious = batch_boxlist_iou(_boxes, _boxes)
-            k_ind = torch.zeros(num_gt_cls.int(), k, dtype=torch.bool, device=device)
-            k_ind[:, 0] = 1 # always take the one with max score 
-            for ii in range(1, k):
-                max_iou, _ = torch.max(ious[:,ii:ii+1, :ii], dim=2)
-                k_ind[:, ii] = (max_iou < self.iou_th).byte().squeeze(-1)
-            
-            gt_boxes = _boxes.bbox[k_ind]
-            gt_cls_id = gt_cls_inds + 1
-            temp_cls = torch.ones((_boxes.bbox.shape[:2]), device=device) * gt_cls_id.view(-1, 1).float()
-            gt_classes = temp_cls[k_ind].view(-1, 1).long()
-            gt_scores = sorted_scores.t().contiguous()[k_ind].view(-1, 1)
-            
-            if gt_boxes.shape[0] != 0:
-                gt_boxes = BoxList(gt_boxes, proposals.size, mode=proposals.mode)
-                overlaps = boxlist_iou(proposals, gt_boxes)
+                _boxes = proposals.bbox[max_inds.t().contiguous().view(-1)].view(num_gt_cls.int(), -1, 4)
+                _boxes = BatchBoxList(_boxes, proposals.size, mode=proposals.mode)
+                ious = batch_boxlist_iou(_boxes, _boxes)
+                # Debug
+                # print("\nIOUS: {}\n".format(ious))
+                k_ind = torch.zeros(num_gt_cls.int(), k, dtype=torch.bool, device=device)
+                # Debug
+                # print("\nK Indexes: {}\n".format(k_ind))
+                k_ind[:, 0] = 1 # always take the one with max score 
+                for ii in range(1, k):
+                    max_iou, _ = torch.max(ious[:,ii:ii+1, :ii], dim=2)
+                    k_ind[:, ii] = (max_iou < self.iou_th).byte().squeeze(-1)
                 
-                # TODO: pytorch and numpy argmax perform differently
-                # max_overlaps, gt_assignment = overlaps.max(dim=1)
-                max_overlaps  = torch.tensor(overlaps.cpu().numpy().max(axis=1), device=device)
-                gt_assignment = torch.tensor(overlaps.cpu().numpy().argmax(axis=1), device=device)
-                                    
-                pseudo_labels = gt_classes[gt_assignment, 0]
-                loss_weights = gt_scores[gt_assignment, 0]
+                gt_boxes = _boxes.bbox[k_ind]
+                gt_cls_id = gt_cls_inds + 1
+                temp_cls = torch.ones((_boxes.bbox.shape[:2]), device=device) * gt_cls_id.view(-1, 1).float()
+                gt_classes = temp_cls[k_ind].view(-1, 1).long()
+                gt_scores = sorted_scores.t().contiguous()[k_ind].view(-1, 1)
                 
-                # fg_inds = max_overlaps.ge(cfg.MODEL.ROI_HEADS.FG_IOU_THRESHOLD).nonzero(as_tuple=False)[:,0]
-                # Select background RoIs as those with <= FG_IOU_THRESHOLD
-                bg_inds = max_overlaps.lt(cfg.MODEL.ROI_HEADS.FG_IOU_THRESHOLD).nonzero(as_tuple=False)[:,0]
-                pseudo_labels[bg_inds] = 0
-                
-                # compute regression targets
-                if return_targets:
-                    matched_targets = gt_boxes[gt_assignment]
-                    regression_targets = self.box_coder.encode(
-                        matched_targets.bbox, proposals.bbox
-                    )
-                    return pseudo_labels, loss_weights, regression_targets
-                
-                return pseudo_labels, loss_weights
+                if gt_boxes.shape[0] != 0:
+                    gt_boxes = BoxList(gt_boxes, proposals.size, mode=proposals.mode)
+                    overlaps = boxlist_iou(proposals, gt_boxes)
+                    
+                    # TODO: pytorch and numpy argmax perform differently
+                    # max_overlaps, gt_assignment = overlaps.max(dim=1)
+                    max_overlaps  = torch.tensor(overlaps.cpu().numpy().max(axis=1), device=device)
+                    gt_assignment = torch.tensor(overlaps.cpu().numpy().argmax(axis=1), device=device)
+                                        
+                    pseudo_labels = gt_classes[gt_assignment, 0]
+                    loss_weights = gt_scores[gt_assignment, 0]
+                    
+                    # fg_inds = max_overlaps.ge(cfg.MODEL.ROI_HEADS.FG_IOU_THRESHOLD).nonzero(as_tuple=False)[:,0]
+                    # Select background RoIs as those with <= FG_IOU_THRESHOLD
+                    bg_inds = max_overlaps.lt(cfg.MODEL.ROI_HEADS.FG_IOU_THRESHOLD).nonzero(as_tuple=False)[:,0]
+                    pseudo_labels[bg_inds] = 0
+                    
+                    # compute regression targets
+                    if return_targets:
+                        matched_targets = gt_boxes[gt_assignment]
+                        regression_targets = self.box_coder.encode(
+                            matched_targets.bbox, proposals.bbox
+                        )
+                        return pseudo_labels, loss_weights, regression_targets
+                    
+                    return pseudo_labels, loss_weights
         
         # corner case
         pseudo_labels = torch.zeros(num_rois, dtype=torch.long, device=device)
